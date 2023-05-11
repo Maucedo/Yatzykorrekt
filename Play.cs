af3826a2c6e6000e8be32978365135a67be760d2
Linus Öst Nielsen <0410LINI@gapp.uddevalla.se>
2023-05-11 17:52:03 +0200

    Börjat skriva kommentarer för att förklara vad koden gör

---
+++ Play.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yatzy_korrekt.Enum;
+
+namespace Yatzy_korrekt
+{
+    public class Play
+    {
+        public int PointValue { get; set; }
+        public NumberOfDices Type { get; set; }
+        
+        public Play(NumberOfDices type, int points)
+        {
+            Type = type;
+            PointValue = points;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using Yatzy_korrekt;
 
+//deklarerar variablar som används i koden
 int slag = 0;
 var random = new Random();
 var tärningar = new List<Dice>(5);
-int rundor = 2;
+int rundor = 4;
 
+
+//Spelloop som då är varje gång en runda körs.
 for (int k = 0; k < rundor; k++)
 {
 
@@ -30,12 +33,16 @@ for (int k = 0; k < rundor; k++)
     Console.ReadLine();
 
     tärnspara = TärningsRunda(random, tärningar, slag);
+
+
     slag = 0;
     tärningar = new List<Dice>(5);
 }
 Console.WriteLine("Spelet är slut");
 Console.ReadLine();
 
+
+//detta är funktionen för vad som händer ska hända under varje slag
 static List<int> TärningsRunda(Random random, List<Dice> tärningar, int slag)
     {
         foreach (Dice y in tärningar)
@@ -45,15 +52,16 @@ static List<int> TärningsRunda(Random random, List<Dice> tärningar, int slag)
         }
         List<int> tärnspara = new List<int>();
         Console.WriteLine();
-        if (slag < 2)
+        if (slag < 2) //Detta är vad som ska hända ifall man inte slått alla slag än
         {
             Console.Write("Vad ska sparas? ");
-            tärnspara = Console.ReadLine().Split(",").Select(int.Parse).ToList();
+            tärnspara = Console.ReadLine().Split(",").Select(int.Parse).ToList(); //frågar efter en input av vilka tärningar man ska spara och lägger dessa i tärnspara, samt bestämmer att inputen måste separeras med ett komma tecken ifall det är mer än ett nummer.
+            //I denna foreach loopen säger vi att varje siffra i tärnspara (vilket är den listan med inputs) så ska vi göra det ekvivalenta elemetet true
             foreach (var t in tärnspara)
             {
                 tärningar[t - 1].spara = true;
             }
-
+            //i denna foreach loopen säger vi att alla tärningar som inte är true ska då "rullas om"
             foreach (var t in tärningar)
             {
                 if (t.spara == false)
@@ -61,18 +69,20 @@ static List<int> TärningsRunda(Random random, List<Dice> tärningar, int slag)
                     t.tärningsnum = random.Next(1, 7);
                 }
 
-                t.spara = false;
+                t.spara = false; //sedan gör vi dessa till false igen så att man kan bestämma sig senare om man vill behålla tärningarna.
             }
 
         }
-        else
+        else //Detta är vad som händer ifall det tredje slaget är färdigt
         {
+            //Skriver ut 1-6 samt hur många av varje tärningsnummer som kom med i resultatet
             var DiceNumbers = Calculate.HowManyOfEachKind(tärningar);
             foreach (var item in DiceNumbers)
             {
                 Console.WriteLine($"{item.Key}: {item.Value}");
             }
 
+            //alla dessa skriver ut ifall man har par, triss osv, å skriver True ifall detta stämmer
             var onepair = Calculate.HasAnyOnePair(tärningar);
             Console.WriteLine($"Ett par: {onepair}");
 
@@ -96,7 +106,7 @@ static List<int> TärningsRunda(Random random, List<Dice> tärningar, int slag)
 
             var TwoPairs = Calculate.HasTwoPairs(tärningar);
             Console.WriteLine($"Two Pairs: {TwoPairs}");
-            Console.ReadLine();
+            Console.ReadLine(); //readline är med här så att koden inte ska upphöra och man kan stanna och läsa vad som står
 
         }
         return tärnspara;
