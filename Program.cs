f1e0c37722df26ef9051e658864a5ff79511f34f
Linus Öst Nielsen <0410LINI@gapp.uddevalla.se>
2023-05-11 17:09:25 +0200

    Har gjort så att man kan spela flera rundor

--- Program.cs
+++ Program.cs
@@ -7,90 +7,102 @@ using Yatzy_korrekt;
 int slag = 0;
 var random = new Random();
 var tärningar = new List<Dice>(5);
-for (int i = 0; i < tärningar.Capacity; i++)
+int rundor = 2;
+
+for (int k = 0; k < rundor; k++)
 {
-    tärningar.Add(new Dice(random.Next(1, 7)));
+
+    for (int i = 0; i < tärningar.Capacity; i++)
+    {
+        tärningar.Add(new Dice(random.Next(1, 7)));
+    }
+
+
+    var tärnspara = TärningsRunda(random, tärningar, slag);
+    slag += 1;
+    Console.WriteLine("Slå igen?");
+    Console.ReadLine();
+
+    tärnspara = TärningsRunda(random, tärningar, slag);
+    slag += 1;
+
+    Console.WriteLine("Slå igen?");
+    Console.ReadLine();
+
+    tärnspara = TärningsRunda(random, tärningar, slag);
+    slag = 0;
+    tärningar = new List<Dice>(5);
 }
-
-
-var tärnspara = TärningsRunda(random, tärningar, slag);
-slag += 1;
-Console.WriteLine("Slå igen?");
+Console.WriteLine("Spelet är slut");
 Console.ReadLine();
-
-tärnspara = TärningsRunda(random, tärningar, slag);
-slag += 1;
-
-Console.WriteLine("Slå igen?");
-Console.ReadLine();
-
-tärnspara = TärningsRunda(random, tärningar, slag);
-
 
 static List<int> TärningsRunda(Random random, List<Dice> tärningar, int slag)
-{
-    foreach (Dice y in tärningar)
     {
-        Console.WriteLine(y.tärningsnum);
-        // detta gör något
-    }
-    List<int> tärnspara = new List<int>();
-    Console.WriteLine();
-    if (slag < 2)
-    {
-        Console.Write("Vad ska sparas? ");
-        tärnspara = Console.ReadLine().Split(",").Select(int.Parse).ToList();
-        foreach (var t in tärnspara)
+        foreach (Dice y in tärningar)
         {
-            tärningar[t - 1].spara = true;
+            Console.WriteLine(y.tärningsnum);
+            // detta gör något
         }
-
-        foreach (var t in tärningar)
+        List<int> tärnspara = new List<int>();
+        Console.WriteLine();
+        if (slag < 2)
         {
-            if (t.spara == false)
+            Console.Write("Vad ska sparas? ");
+            tärnspara = Console.ReadLine().Split(",").Select(int.Parse).ToList();
+            foreach (var t in tärnspara)
             {
-                t.tärningsnum = random.Next(1, 7);
+                tärningar[t - 1].spara = true;
             }
 
-            t.spara = false;
-        }
+            foreach (var t in tärningar)
+            {
+                if (t.spara == false)
+                {
+                    t.tärningsnum = random.Next(1, 7);
+                }
 
-    }
-    else
-    {
-        var DiceNumbers = Calculate.HowManyOfEachKind(tärningar);
-        foreach (var item in DiceNumbers)
+                t.spara = false;
+            }
+
+        }
+        else
         {
-            Console.WriteLine($"{item.Key}: {item.Value}");
+            var DiceNumbers = Calculate.HowManyOfEachKind(tärningar);
+            foreach (var item in DiceNumbers)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+
+            var onepair = Calculate.HasAnyOnePair(tärningar);
+            Console.WriteLine($"Ett par: {onepair}");
+
+            var ThreeOfAKind = Calculate.HasAnyThreeOfAKind(tärningar);
+            Console.WriteLine($"Triss: {ThreeOfAKind}");
+
+            var FourOfAKind = Calculate.HasAnyFourOfAKind(tärningar);
+            Console.WriteLine($"Fyrtal: {FourOfAKind}");
+
+            var Yatzy = Calculate.HasAnyYatzy(tärningar);
+            Console.WriteLine($"Yatzy: {Yatzy}");
+
+            var FullHouse = Calculate.HasFullHouse(tärningar);
+            Console.WriteLine($"Fullhouse: {FullHouse}");
+
+            var TinyLadder = Calculate.HasSmallLadder(tärningar);
+            Console.WriteLine($"Small Ladder: {TinyLadder}");
+
+            var BigLadder = Calculate.HasBigLadder(tärningar);
+            Console.WriteLine($"Big Ladder: {BigLadder}");
+
+            var TwoPairs = Calculate.HasTwoPairs(tärningar);
+            Console.WriteLine($"Two Pairs: {TwoPairs}");
+            Console.ReadLine();
+
         }
+        return tärnspara;
 
-        var onepair = Calculate.HasAnyOnePair(tärningar);
-        Console.WriteLine($"Ett par: {onepair}");
-
-        var ThreeOfAKind = Calculate.HasAnyThreeOfAKind(tärningar);
-        Console.WriteLine($"Triss: {ThreeOfAKind}");
-
-        var FourOfAKind = Calculate.HasAnyFourOfAKind(tärningar);
-        Console.WriteLine($"Fyrtal: {FourOfAKind}");
-
-        var Yatzy = Calculate.HasAnyYatzy(tärningar);
-        Console.WriteLine($"Yatzy: {Yatzy}");
-
-        var FullHouse = Calculate.HasFullHouse(tärningar);
-        Console.WriteLine($"Fullhouse: {FullHouse}");
-
-        var TinyLadder = Calculate.HasSmallLadder(tärningar);
-        Console.WriteLine($"Small Ladder: {TinyLadder}");
-
-        var BigLadder = Calculate.HasBigLadder(tärningar);
-        Console.WriteLine($"Big Ladder: {BigLadder}");
-
-        var TwoPairs = Calculate.HasTwoPairs(tärningar);
-        Console.WriteLine($"Two Pairs: {TwoPairs}");
-        Console.ReadLine(); 
-        
     }
-    return tärnspara;
-}
-    
+
+
+
 
